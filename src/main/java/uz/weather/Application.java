c4d54f097899e16d41293a7d6f7df3cf3527c92a
jack-sparrow-18-04 <akhmedovkhasanboy28@gmail.com>
2024-03-24 12:05:55 +0500

    ToDo added

--- src/main/java/uz/weather/Application.java
+++ src/main/java/uz/weather/Application.java
@@ -2,6 +2,10 @@ package uz.weather;
 
 public class Application {
     public static void main(String[] args) {
+/*
+    TODO Xusanboy
+     Weather Api projectini Console variantini tuzish
+ */
 
     }
 }
